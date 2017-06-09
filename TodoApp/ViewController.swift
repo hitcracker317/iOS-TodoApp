a9ca0bfe4a863e733a7e566124e43eab75ea7e1c
hitcrakcer317 <dainaso97@gmail.com>
2017-06-09 10:47:27 +0900

    追加したtodoを削除する処理を実装

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -86,26 +86,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return 64
     }
     
-    //データの操作
-    func addTodo(todoText:String) {
-        //追加
-        let todoData = Todo()
-        todoData.todoText = todoText
-        todoData.isChecked = false
-        
-        self.todoArray.insert(todoData, at: 0)
-        
-        
-        //テーブルに行を追加
-        self.todoTableView.insertRows(at: [IndexPath(row: 0, section:0)], with: UITableViewRowAnimation.right)
-        
-        saveTodo()
-    }
     
     //セルを選択したとき
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let todo = self.todoArray[indexPath.row] as! Todo
+        let todo = todoArray[indexPath.row] as! Todo
         
         //チェックマーク
         if todo.isChecked {
@@ -119,11 +104,46 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         saveTodo()
     }
     
+    //セルが編集可能かどうか
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //セルの削除ボタンをタップした際に呼ばれる
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        //削除可能かどうか
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            //todo削除
+            todoArray.remove(at: indexPath.row)
+
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+
+            saveTodo ()
+        }
+    }
+    
+    //データの操作
+    func addTodo(todoText:String) {
+        //追加
+        let todoData = Todo()
+        todoData.todoText = todoText
+        todoData.isChecked = false
+        
+        todoArray.insert(todoData, at: 0)
+        
+        //テーブルに行を追加
+        self.todoTableView.insertRows(at: [IndexPath(row: 0, section:0)], with: UITableViewRowAnimation.right)
+        
+        saveTodo()
+    }
+    
     func saveTodo () {
         //データ保存
         let ud = UserDefaults.standard
-        let archiveData = NSKeyedArchiver.archivedData(withRootObject: self.todoArray)
+        let archiveData = NSKeyedArchiver.archivedData(withRootObject: todoArray)
         ud.set(archiveData, forKey: "todo")
+        ud.synchronize()
     }
 }
 
